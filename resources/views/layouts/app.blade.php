f4a5385c58b193130df36bbb834e6cdd9746a518
rully <hvsciverius@gmail.com>
2020-07-18 14:40:11 +0700

    removed most of the footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,25 +19,15 @@
             <div class="container">
                 <div class="row">
                     <div class="col-md-3 col-6">
-                        <div class="mb-3">&copy; {{ env('APP_NAME') }}</div>
-
-                        <ul class="list-unstyled">
-                            <li><a href="{{ route('partner.login') }}">Login</a></li>
-                            <li><a href="{{ route('partner.register') }}">Daftar</a></li>
-                        </ul>
+                        <div class="mb-3">&copy; {{ now()->year }} {{ env('APP_NAME') }}</div>
                     </div>
                     <div class="col-md-3 col-6">
                         <ul class="list-unstyled">
-                            <a href="{{ route('articles.index') }}">{{ __('pages.articles') }}</a>
                             @php $pages = \App\Post::pages()->get(); @endphp
                             @foreach($pages as $page)
                                 <li><a href="{{ route('page', ['slug' => $page->slug]) }}">{{ $page->name }}</a></li>
                             @endforeach
-                            <li><a href="{{ route('page', ['slug' => 'tentang']) }}">Tentang</a></li>
-                            <li><a href="{{ route('page', ['slug' => 'panduan']) }}">Panduan</a></li>
-                            <li><a href="{{ route('page', ['slug' => 'tips']) }}">Tips</a></li>
-                            <li><a href="{{ route('page', ['slug' => 'hubungi-kami']) }}">Hubungi Kami</a></li>
-                            <li><a href="{{ route('page', ['slug' => 'syarat']) }}">Syarat &amp; Ketentuan</a></li>
+                            <li><a href="{{ route('page', ['slug' => 'tentang']) }}">Tentang Kami</a></li>
                         </ul>
                     </div>
                 </div>
